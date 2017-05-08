89b534625f9f1c2f9cda0a72886755575152cd8e
MoveZZG <zhuzhaogong@163.com>
2017-05-08 08:49:36 +0800

    'index'

--- src/scripts/components/Board.vue
+++ src/scripts/components/Board.vue
@@ -1,21 +1,34 @@
 <template lang="html">
   <div class="">
-    <div class="yo-header">
-      <span class="regret yo-ico">&#xe622;</span>
-      <h2 class="title">
-        <div class="">
-          <span class="yo-ico">&#xe511;</span>
-          <input type="text" name="" value="">
+    <div class="yo-header-index">
+      <div class="regret flc">
+        <span class="yo-ico">&#xe622;</span>
+        <span>客服</span>
+      </div>
+      <h2 class="searchBox">
+          <div class="sousuo">
+              <span class="yo-ico">&#xe511;</span>
+          </div>
+          <input type="text" name="" value="" placeholder="搜索">
           <span class="yo-ico">&#xe617;</span>
-        </div>
       </h2>
-      <span class="affirm yo-ico">&#xf07e;</span>
+      <div class="affirm flc">
+        <span class="yo-ico">&#xe604;</span>
+        <span>信息</span>
+      </div>
     </div>
-    <mt-swipe :auto="6000">
+    <!-- <mt-swipe :auto="6000">
       <mt-swipe-item v-for="(item,index) in dataSource" :keys='index'>
         <img :src="item.goods_thumb" alt="">
       </mt-swipe-item>
-    </mt-swipe>
+    </mt-swipe> -->
+    <section class="banner">
+      <div  v-for="(item,index) in dataSource">
+        <router-link :to="{path:''}">
+          <a><img :src="item.ads[0].ad_img" alt=""></a>
+        </router-link>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -36,10 +49,11 @@
     mounted:function(){
       let that = this;
       axiosUtil.get({
-        url:'api/goods/homeGoods',
+        url:'api/home/index3',
         type:'get',
         callback:(res)=>{
-          that.dataSource = this.dataSource.concat(res.data.body.datas)
+          console.log(res.data.body);
+          that.dataSource = this.dataSource.concat(res.data.body);
         }
       })
     }
@@ -47,10 +61,69 @@
 </script>
 
 <style scoped lang='scss'>
-.mint-swipe{
-  height: 1.9rem;
-  .mint-swipe-item{
-    background: red;
+.yo-header-index{
+  width: 100%;
+  position: fixed;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  background: transparent;
+  overflow: hidden;
+  margin: .04rem 0;
+  .flc{
+    display: flex;
+    width: .44rem;
+    flex-direction: column;
+    align-items:flex-start;
+    font-size: .11rem;
+    color: #fff;
+    span{
+      width: 100%;
+      text-align: center;
+      display: block;
+    }
+    .yo-ico{
+      font-size: .15rem;
+    }
+  }
+  .searchBox{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background: #ececec;
+    box-sizing: border-box;
+    border: .1px #e4f4f3 solid;
+    width: 2.8rem;
+    font-size: .15rem;
+    border-radius: .05rem .05rem;
+    .yo-ico{
+      font-size: .15rem;
+      color: #c4c4c4
+    }
+    input{
+      color: #565656;
+      background: transparent;
+      height: .23rem;
+      border: 0;
+      margin: 0 .1rem;
+    }
+  }
+}
+// .mint-swipe{
+//   height: 1.9rem;
+//   .mint-swipe-item{
+//     background: red;
+//     img{
+//       width: 100%;
+//     }
+//   }
+// }
+.banner{
+  height: 2.25rem;
+  width: 100%;
+  div{
+    height: 100%;
     img{
       width: 100%;
     }
