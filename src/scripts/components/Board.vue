4807c3142c4e9ad69da4acfa1da7353896cd436d
MoveZZG <zhuzhaogong@163.com>
2017-05-08 22:05:15 +0800

    '主页'

--- src/scripts/components/Board.vue
+++ src/scripts/components/Board.vue
@@ -1,5 +1,6 @@
 <template lang="html">
   <div class="">
+
     <div class="yo-header-index">
       <div class="regret flc">
         <span class="yo-ico">&#xe622;</span>
@@ -17,43 +18,92 @@
         <span>信息</span>
       </div>
     </div>
-    <!-- <mt-swipe :auto="6000">
-      <mt-swipe-item v-for="(item,index) in dataSource" :keys='index'>
-        <img :src="item.goods_thumb" alt="">
-      </mt-swipe-item>
-    </mt-swipe> -->
-    <section class="banner">
-      <div  v-for="(item,index) in dataSource">
-        <router-link :to="{path:''}">
-          <a><img :src="item.ads[0].ad_img" alt=""></a>
-        </router-link>
-      </div>
-    </section>
+    <mt-loadmore v-if="hasdata" :top-method="loadTop" :topDropText="'释放刷新'"  :bottom-all-loaded="allLoaded" :autoFill="false" ref="loadmore">
+      <section class="banner">
+        <div  v-for="(item,index) in dataSource.ads" >
+          <router-link  :to="item.ad_url">
+            <a><img :src="item.ad_img" alt=""></a>
+          </router-link>
+        </div>
+      </section>
+      <section class="fenlei">
+          <dl class=""  v-for="(item,index) in dataSource.shortcuts">
+            <router-link  :to="item.url">
+              <dt><img :src="item.icon" :alt="item.name"></dt>
+            </router-link>
+            <dd v-text="item.name"></dd>
+          </dl>
+      </section>
+      <section class="huodong">
+          <div v-for="(item,index) in dataSource.modules.arr">
+             <router-link  :to="item.url">
+               <img :src="item.img">
+             </router-link>
+          </div>
+      </section>
+      <section class="jiugongge" v-for="(item,index) in dataSource.goods_jiugongge">
+        <div class="left">
+         <router-link :to="item.left.url">
+           <img :src="item.left.img" alt="item.left.name">
+         </router-link>
+        </div>
+        <div class="right">
+          <div class="item" v-for="(item1,index) in item.right" >
+            <router-link :to="item1.url">
+              <img :src="item1.img" alt="item1.name">
+            </router-link>
+          </div>
+        </div>
+        <div class="bottom">
+         <router-link :to="item.bottom.url">
+           <img :src="item.bottom.img" alt="item.bottom.name">
+         </router-link>
+        </div>
+      </section>
+    </mt-loadmore>
   </div>
 </template>
 
 <script>
   import Vue from 'vue';
   import { Swipe, SwipeItem } from 'mint-ui';
-  // import 'mint-ui/lib/style.css'
+  import axiosUtil from '../utils/axios';
+  import { Loadmore } from 'mint-ui';
+  import { Indicator } from 'mint-ui';
   Vue.component(Swipe.name, Swipe);
   Vue.component(SwipeItem.name, SwipeItem);
-
-  import axiosUtil from '../utils/axios';
+  Vue.component(Loadmore.name, Loadmore);
   export default {
     data(){
       return{
-        dataSource:[]
+        dataSource:{},
+        allLoaded:false,
+        hasdata:false
+      }
+    },
+    methods:{
+      loadTop: function () {
+        let that = this;
+        setTimeout(function(){
+          that.$refs.loadmore.onTopLoaded();
+        }.bind(this),3000)
       }
     },
     mounted:function(){
+      Indicator.open({
+        text: '加载中...',
+        spinnerType: 'fading-circle'
+      });
       let that = this;
       axiosUtil.get({
         url:'api/home/index3',
         type:'get',
         callback:(res)=>{
-          console.log(res.data.body);
-          that.dataSource = this.dataSource.concat(res.data.body);
+          that.dataSource = res.data.body;
+          this.hasdata = true;
+          setTimeout(()=>{
+            Indicator.close();
+          },500)
         }
       })
     }
@@ -70,6 +120,7 @@
   background: transparent;
   overflow: hidden;
   margin: .04rem 0;
+  z-index: 9999;
   .flc{
     display: flex;
     width: .44rem;
@@ -97,6 +148,7 @@
     width: 2.8rem;
     font-size: .15rem;
     border-radius: .05rem .05rem;
+    opacity: 0.8;
     .yo-ico{
       font-size: .15rem;
       color: #c4c4c4
@@ -120,13 +172,83 @@
 //   }
 // }
 .banner{
-  height: 2.25rem;
   width: 100%;
   div{
     height: 100%;
+    a{
+      display: block;
+      height: 100%;
+      img{
+        width: 100%;
+      }
+    }
+  }
+}
+// 分类
+.fenlei{
+  display: flex;
+  justify-content:space-around;
+  flex-wrap:wrap;
+  background: #fff;
+  margin-bottom: .02rem;
+  dl{
+    width: 25%;
+    text-align: center;
     img{
+      width: .45rem;
+    }
+  }
+  dd{
+    padding-top: .06rem;
+    font-size: .13rem;
+    text-align: center;
+  }
+}
+//活动
+.huodong{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: .1rem;
+  overflow: hidden;
+  >div{
+    width: 33%;
+    height: .62rem;
+    a{display: block;}
+    img{
+      display: block;
       width: 100%;
     }
+  }
+}
+.jiugongge{
+  display: flex;
+  overflow: hidden;
+  justify-content: space-between;
+  flex-direction: row;
+  flex-wrap: wrap;
+  a{
+    display: block;
+    img{
+        width: 100%;
+    }
+  }
+  .left{
+    width: 38%;
+  }
+  .right{
+    width: 62%;
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    .item{
+      height: 50%;
+      width: 49%;
+    }
+  }
+  .bottom{
+    width: 100%;
   }
 }
 </style>
